b5592bd8a899cd0da0ec053819c22da33ae44f6e
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-20 16:17:33 +0200

    FEAT new view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div>LOGONE PAZZESCO</div>
+            <ul class="link-list">
+                <li><a href="">Chi siamo: AboutUs</a></li>
+                <li><a href="">Cosa facciamo: Mission</a></li>
+                <li><a href="">Con chi lo facciamo: Sponsor</a></li>
+                <li><a href="">Cosa vogliamo da te: Donation</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1> Hi to you!</h1>
+</body>
+
+</html>
