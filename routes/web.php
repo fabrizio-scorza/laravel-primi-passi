d47cd9a902a57c79a77957441d2d9d1b2e260d09
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-23 15:37:12 +0200

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about-us', function () {
     return view('about-us');
 })->name('about');
